e363754c3680eb1f9ba3690e3573e230128ac9e8
lee <lee@lee-PC>
2018-05-04 09:58:06 +0900

    2018.05.04

---
+++ BrainJava/src/SubscriberInfo.java
@@ -0,0 +1,37 @@
+/**
+ * 2018. 5. 3. Dev By Lee.S.J
+ * 
+ * BrainJava
+ */
+
+/**
+ * @author lee
+ *
+ */
+public class SubscriberInfo {
+	String name, id, password;
+	String phoneNo, address;
+	SubscriberInfo(String name, String id, String password) {
+		this.name = name;
+		this.id = id;
+		this.password = password;
+	}
+	SubscriberInfo(String name, String id, String password, String phoneNo, String address) {
+		this.name =name;
+		this.id = id;
+		this.password = password;
+		this.phoneNo = phoneNo;
+		this.address = address;
+		
+	}
+	void changePassword(String password) {
+		this.password = password;
+	}
+	void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	void setAddress (String address) {
+		this.address = address;
+	}
+
+}
